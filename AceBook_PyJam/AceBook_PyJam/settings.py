d8e1d2d1eff464e93fdc9a3431684bba7065af05
Aaron Diable <aaron.diable@gmail.com>
2020-02-05 13:31:52 +0000

    Inital landing page

--- AceBook_PyJam/AceBook_PyJam/settings.py
+++ AceBook_PyJam/AceBook_PyJam/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'AceBook_PyJam.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'AceBook_PyJam.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'pyjam',
+        'USER': 'aaron',
+        'PASSWORD': ' ',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
