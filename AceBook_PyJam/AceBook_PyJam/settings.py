f3c0bf4ec8d91adad850c71d21470f1e00d1a48f
Aaron Diable <aaron.diable@gmail.com>
2020-02-12 16:35:27 +0000

    Push after git ignore

--- AceBook_PyJam/AceBook_PyJam/settings.py
+++ AceBook_PyJam/AceBook_PyJam/settings.py
@@ -78,7 +78,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'pyjam',
-        'USER': 'student',
+        'USER': 'aaron',
         'PASSWORD': ' ',
         'HOST': '127.0.0.1',
         'PORT': '5432',
