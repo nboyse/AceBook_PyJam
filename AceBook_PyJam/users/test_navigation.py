89ba64e7695f80f86eaa602945ae6e12a2d63c70
nboyse <natasha.boyse@trade.gov.uk>
2020-02-12 14:29:29 +0000

    Update test_navigation.py

--- AceBook_PyJam/users/test_navigation.py
+++ AceBook_PyJam/users/test_navigation.py
@@ -4,6 +4,9 @@ from .views import home
 from .views import log_in
 from django.contrib.auth.models import User
 from django.test import Client
+from django.db import models
+from .models import Posts
+
 
 # Create your tests here.
 
@@ -14,7 +17,13 @@ class TestNavigation(TestCase):
 
     def tearDown(self):
         pass
-        
+
     def test_root_url_resolves_to_index_view(self):
         found = resolve('/')
         self.assertEqual(found.func, home)
+
+    def test_valid_user(self):
+        u = User.objects.create(username='test3', password='Bar')
+        data = {'username': u.username, 'password': u.password,}
+        form = Posts
+        self.assertTrue(form)
